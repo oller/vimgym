4baab5d49b426b135bfb4132f37cda0367700bbd
David Ollerhead <david.ollerhead@gmail.com>
2025-12-12 23:33:12 +0000

    chore: Tidyup

--- src/components/VimEditor.keypress.test.tsx
+++ src/components/VimEditor.keypress.test.tsx
@@ -16,30 +16,7 @@ describe("VimEditor Keypress Logging", () => {
       highScores: {},
     });
 
-    // Mock JSDOM layout methods
-    document.createRange = () => {
-      const range = new Range();
-      range.getBoundingClientRect = () => ({
-        x: 0,
-        y: 0,
-        width: 0,
-        height: 0,
-        top: 0,
-        right: 0,
-        bottom: 0,
-        left: 0,
-        toJSON: () => {},
-      });
-      range.getClientRects = () => ({
-        length: 0,
-        item: () => null,
-        [Symbol.iterator]: [][Symbol.iterator],
-      });
-      return range;
-    };
-  });
-
-  it("logs keypresses from insert mode", async () => {
+    it("logs keypresses from insert mode", async () => {
     render(<VimEditor />);
 
     // Get the editor container
